71904248a4cdce14e6ba9bc88c3a885f9fa811a4
yoneyama ryo <uryonym@gmail.com>
2022-06-10 11:35:53 +0900

    add offline access at firestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
-import { initializeApp, FirebaseOptions } from 'firebase/app'
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import {initializeApp, FirebaseOptions} from 'firebase/app'
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore, enableMultiTabIndexedDbPersistence} from 'firebase/firestore'
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -13,3 +13,16 @@ export const firebaseApp = initializeApp(firebaseConfig)
 export const googleProvider = new GoogleAuthProvider()
 export const firebaseAuth = getAuth(firebaseApp)
 export const firebaseDb = getFirestore(firebaseApp)
+
+enableMultiTabIndexedDbPersistence(firebaseDb).catch((e) => {
+  if (e.code == 'failed-precondition') {
+    console.log('failed-precondition')
+    console.log(e)
+  } else if (e.code == 'unimplemented') {
+    console.log('unimplemented')
+    console.log(e)
+  } else {
+    console.log('unknown')
+    console.log(e)
+  }
+})
