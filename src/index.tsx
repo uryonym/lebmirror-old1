a2587b4c3b0879c3738d7f3f4bc3890707f5f2a8
yoneyama ryo <uryonym@gmail.com>
2022-06-18 23:20:53 +0900

    integrate tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { App } from './App'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { BrowserRouter } from 'react-router-dom'
+import './style/index.scss'
 
 const container = document.getElementById('root')
 createRoot(container).render(
