7f2ddb2126ead05565eb68e0afe18f91dd842360
yoneyama ryo <uryonym@gmail.com>
2022-06-19 10:18:28 +0900

    install and setup preline-ui

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { App } from './App'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { BrowserRouter } from 'react-router-dom'
+import 'preline/dist/hs-ui.bundle'
 import './style/index.css'
 
 const container = document.getElementById('root')
