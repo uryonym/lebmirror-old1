8b928c3e33a10633bfe4aad55aea1f6f8c7dad0d
yoneyama ryo <uryonym@gmail.com>
2022-06-23 23:14:04 +0900

    set new-form and list

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -29,6 +29,7 @@ export const Home = () => {
       createdAt: new Date(),
     }
     dispatch(createSection(data))
+    setSectionName('')
   }
 
   const handleInputRenameSection = (e: ChangeEvent<HTMLInputElement>) => {
@@ -70,6 +71,7 @@ export const Home = () => {
       createdAt: new Date(),
     }
     dispatch(createPage(data))
+    setPageName('')
   }
 
   const handleInputRenamePage = (e: ChangeEvent<HTMLInputElement>) => {
@@ -98,85 +100,127 @@ export const Home = () => {
     dispatch(destroyPage(pageId))
   }
 
-  return (
-    <ReflexContainer orientation='vertical'>
-      <ReflexElement>
-        <div>
+  if (currentNote) {
+    return (
+      <ReflexContainer orientation='vertical'>
+        <ReflexElement>
           <div>
-            <input type='test' value={sectionName} onChange={handleInputSectionName} />
-            <button type='button' className='outline-btn' onClick={handleCreateSection}>
-              セクション作成
-            </button>
-          </div>
-          {renameSection && (
             <div>
-              <input type='text' value={renameSection.name} onChange={handleInputRenameSection} />
-              <button type='button' onClick={handleRenameSection}>
-                名前を更新
-              </button>
-              <button type='button' onClick={handleCancelRenameSection}>
-                キャンセル
-              </button>
+              <label htmlFor='hs-trailing-button-add-on' className='sr-only'>
+                Label
+              </label>
+              <div className='flex rounded-md shadow-sm m-2'>
+                <input
+                  type='text'
+                  id='hs-trailing-button-add-on'
+                  name='hs-trailing-button-add-on'
+                  className='py-2 px-3 block w-full border-gray-200 shadow-sm rounded-l-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500'
+                  value={sectionName}
+                  onChange={handleInputSectionName}
+                />
+                <button
+                  type='button'
+                  className='py-2 px-3 inline-flex flex-shrink-0 justify-center items-center gap-2 rounded-r-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm'
+                  onClick={handleCreateSection}
+                >
+                  作成
+                </button>
+              </div>
             </div>
-          )}
-          <ul>
-            {sections.map((section: Section) => (
-              <li key={section.id}>
-                <a href='#' onClick={() => handleSelectSection(section.id)}>
-                  {section.name}
-                </a>
-                <button type='button' onClick={() => handleUpdateSection(section.id)}>
-                  名前変更
+            {renameSection && (
+              <div>
+                <input type='text' value={renameSection.name} onChange={handleInputRenameSection} />
+                <button type='button' onClick={handleRenameSection}>
+                  名前を更新
                 </button>
-                <button type='button' onClick={() => handleDeleteSection(section.id)}>
-                  削除
+                <button type='button' onClick={handleCancelRenameSection}>
+                  キャンセル
                 </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </ReflexElement>
-      <ReflexSplitter />
-      <ReflexElement>
-        <div>
+              </div>
+            )}
+            <ul className='max-w-xs flex flex-col m-2'>
+              {sections.map((section: Section) => (
+                <li
+                  key={section.id}
+                  className='inline-flex items-center gap-x-2 py-2 px-3 text-sm font-medium bg-white border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg'
+                >
+                  <a href='#' onClick={() => handleSelectSection(section.id)}>
+                    {section.name}
+                  </a>
+                  <button type='button' onClick={() => handleUpdateSection(section.id)}>
+                    名前変更
+                  </button>
+                  <button type='button' onClick={() => handleDeleteSection(section.id)}>
+                    削除
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </ReflexElement>
+        <ReflexSplitter />
+        <ReflexElement>
           <div>
-            <input type='test' value={pageName} onChange={handleInputPageName} />
-            <button type='button' className='outline-btn' onClick={handleCreatePage}>
-              ページ作成
-            </button>
-          </div>
-          {renamePage && (
             <div>
-              <input type='text' value={renamePage.name} onChange={handleInputRenamePage} />
-              <button type='button' onClick={handleRenamePage}>
-                名前を更新
-              </button>
-              <button type='button' onClick={handleCancelRenamePage}>
-                キャンセル
-              </button>
+              <label htmlFor='hs-trailing-button-add-on' className='sr-only'>
+                Label
+              </label>
+              <div className='flex rounded-md shadow-sm m-2'>
+                <input
+                  type='text'
+                  id='hs-trailing-button-add-on'
+                  name='hs-trailing-button-add-on'
+                  className='py-2 px-3 block w-full border-gray-200 shadow-sm rounded-l-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500'
+                  value={pageName}
+                  onChange={handleInputPageName}
+                />
+                <button
+                  type='button'
+                  className='py-2 px-3 inline-flex flex-shrink-0 justify-center items-center gap-2 rounded-r-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm'
+                  onClick={handleCreatePage}
+                >
+                  作成
+                </button>
+              </div>
             </div>
-          )}
-          <ul>
-            {pages.map((page: Page) => (
-              <li key={page.id}>
-                <a href='#' onClick={() => handleSelectPage(page.id)}>
-                  {page.name}
-                </a>
-                <button type='button' onClick={() => handleUpdatePage(page.id)}>
-                  名前変更
+            {renamePage && (
+              <div>
+                <input type='text' value={renamePage.name} onChange={handleInputRenamePage} />
+                <button type='button' onClick={handleRenamePage}>
+                  名前を更新
                 </button>
-                <button type='button' onClick={() => handleDeletePage(page.id)}>
-                  削除
+                <button type='button' onClick={handleCancelRenamePage}>
+                  キャンセル
                 </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </ReflexElement>
-      <ReflexSplitter />
-      <ReflexElement>
-        <LebEditor content={currentPage?.content} />
-      </ReflexElement>
-    </ReflexContainer>
-  )
+              </div>
+            )}
+            <ul className='max-w-xs flex flex-col m-2'>
+              {pages.map((page: Page) => (
+                <li
+                  key={page.id}
+                  className='inline-flex items-center gap-x-2 py-2 px-3 text-sm font-medium bg-white border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg'
+                >
+                  <a href='#' onClick={() => handleSelectPage(page.id)}>
+                    {page.name}
+                  </a>
+                  <button type='button' onClick={() => handleUpdatePage(page.id)}>
+                    名前変更
+                  </button>
+                  <button type='button' onClick={() => handleDeletePage(page.id)}>
+                    削除
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </ReflexElement>
+        <ReflexSplitter />
+        <ReflexElement>
+          <LebEditor content={currentPage?.content} />
+        </ReflexElement>
+      </ReflexContainer>
+    )
+  } else {
+    return <p>ノートを選択してください。</p>
+  }
 }
